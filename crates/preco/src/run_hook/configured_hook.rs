f33cdeb8a909545a40baebd8b21cf6de49a09bf3
Aarni Koskela <akx@iki.fi>
2024-02-22 13:02:35 +0200

    Make it harder to forget an override

--- crates/preco/src/run_hook/configured_hook.rs
+++ crates/preco/src/run_hook/configured_hook.rs
@@ -1,4 +1,4 @@
-use crate::cfg::pre_commit_config::HookConfiguration;
+use crate::cfg::pre_commit_config::{HookConfiguration, HookDefinitionOverrides};
 use crate::cfg::pre_commit_hooks::HookDefinition;
 use crate::checkout::LoadedCheckout;
 use anyhow::{anyhow, Result};
@@ -21,36 +21,46 @@ pub(crate) fn configure_hook(
     let HookConfiguration {
         id,
         info: _,
-        overrides: o,
+        overrides,
     } = &hc;
 
-    if o.exclude_types.is_some() {
+    let HookDefinitionOverrides {
+        name,
+        description,
+        files,
+        exclude,
+        types,
+        types_or,
+        exclude_types,
+        additional_dependencies,
+        args,
+        stages,
+        always_run,
+    } = &overrides;
+
+    if exclude_types.is_some() {
         warn!(
             "not implemented: exclude_types; not honoring {:?}",
-            o.exclude_types
+            exclude_types
         );
     }
 
     let merged_hook_def = HookDefinition {
         id: id.clone(),
-        name: o.name.clone().unwrap_or_else(|| d.name.clone()),
-        description: o
-            .description
-            .clone()
-            .unwrap_or_else(|| d.description.clone()),
+        name: name.clone().unwrap_or_else(|| d.name.clone()),
+        description: description.clone().unwrap_or_else(|| d.description.clone()),
         entry: d.entry.clone(),
-        args: o.args.clone().unwrap_or_else(|| d.args.clone()),
+        args: args.clone().unwrap_or_else(|| d.args.clone()),
         language: d.language.clone(),
-        stages: o.stages.clone().or_else(|| d.stages.clone()),
-        types: o.types.clone().or_else(|| d.types.clone()),
-        types_or: o.types_or.clone().or_else(|| d.types_or.clone()),
-        files: o.files.clone().or_else(|| d.files.clone()),
-        exclude: o.exclude.clone().or_else(|| d.exclude.clone()),
+        stages: stages.clone().or_else(|| d.stages.clone()),
+        types: types.clone().or_else(|| d.types.clone()),
+        types_or: types_or.clone().or_else(|| d.types_or.clone()),
+        files: files.clone().or_else(|| d.files.clone()),
+        exclude: exclude.clone().or_else(|| d.exclude.clone()),
         pass_filenames: d.pass_filenames,
-        always_run: o.always_run.unwrap_or(d.always_run),
+        always_run: always_run.unwrap_or(d.always_run),
         require_serial: d.require_serial,
-        additional_dependencies: o
-            .additional_dependencies
+        additional_dependencies: additional_dependencies
             .clone()
             .unwrap_or_else(|| d.additional_dependencies.clone()),
         minimum_pre_commit_version: d.minimum_pre_commit_version.clone(),
