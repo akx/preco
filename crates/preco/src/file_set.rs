f2bf015ce9805e328eda5f501c9844f62d79f822
Aarni Koskela <akx@iki.fi>
2024-02-25 15:58:06 +0200

    Fix the bottleneck I foresaw

--- crates/preco/src/file_set.rs
+++ crates/preco/src/file_set.rs
@@ -2,26 +2,69 @@ use crate::git::files::{get_git_tracked_files, get_staged_files, get_unstaged_fi
 use anyhow::bail;
 use git2::Repository;
 use identify::mappings::{map_extension, map_name};
-use std::collections::HashMap;
-use std::path::PathBuf;
+use rustc_hash::{FxHashMap, FxHashSet};
+use std::path::{Path, PathBuf};
 use std::rc::Rc;
-use tracing::{debug, instrument};
+use tracing::{instrument};
 
 #[derive(Debug)]
 pub(crate) struct FileSet {
     pub(crate) root_path: PathBuf,
     pub(crate) files: Vec<Rc<PathBuf>>,
-    pub(crate) files_by_type: HashMap<String, Vec<Rc<PathBuf>>>,
+    #[allow(dead_code)]
+    pub(crate) files_by_type: FxHashMap<String, Vec<Rc<PathBuf>>>,
+    pub(crate) types_by_file: FxHashMap<Rc<PathBuf>, FxHashSet<String>>,
 }
 
 impl FileSet {
     pub(crate) fn has_type(&self, file: &Rc<PathBuf>, typename: &str) -> bool {
-        // TODO: this is a probable bottleneck, should probably prebuild
-        //       a mapping from file -> types too
-        self.files_by_type
-            .get(typename)
-            .map(|files| files.contains(file))
+        self.types_by_file
+            .get(file)
+            .map(|types| types.contains(typename))
             .unwrap_or(false)
+    }
+
+    pub(crate) fn from_raw_files(root_path: &Path, files: Vec<PathBuf>) -> anyhow::Result<FileSet> {
+        let files: Vec<Rc<PathBuf>> = files.into_iter().map(Rc::new).collect();
+        let mut files_by_type: FxHashMap<String, Vec<Rc<PathBuf>>> = FxHashMap::default();
+        files_by_type.reserve(files.len());
+        let mut types_by_file: FxHashMap<Rc<PathBuf>, FxHashSet<String>> = FxHashMap::default();
+        for file in &files {
+            if let Some(ext) = file.extension().and_then(|s| s.to_str()) {
+                if let Some((n, types)) = map_extension(ext) {
+                    for typename in &types[0..(*n)] {
+                        files_by_type
+                            .entry(typename.to_string())
+                            .or_default()
+                            .push(Rc::clone(file));
+                        types_by_file
+                            .entry(Rc::clone(file))
+                            .or_default()
+                            .insert(typename.to_string());
+                    }
+                }
+            }
+            if let Some(name) = file.file_name().and_then(|s| s.to_str()) {
+                if let Some((n, types)) = map_name(name) {
+                    for typename in &types[0..(*n)] {
+                        files_by_type
+                            .entry(typename.to_string())
+                            .or_default()
+                            .push(Rc::clone(file));
+                        types_by_file
+                            .entry(Rc::clone(file))
+                            .or_default()
+                            .insert(typename.to_string());
+                    }
+                }
+            }
+        }
+        Ok(FileSet {
+            root_path: root_path.to_path_buf(),
+            files,
+            files_by_type,
+            types_by_file,
+        })
     }
 }
 
@@ -42,34 +85,5 @@ pub(crate) fn get_file_set(root_path: &PathBuf, all_files: bool) -> anyhow::Resu
         }
         get_staged_files(&repo)?
     };
-    debug!("files_raw: {:?}", files_raw);
-    let files: Vec<Rc<PathBuf>> = files_raw.into_iter().map(Rc::new).collect();
-    let mut files_by_type: HashMap<String, Vec<Rc<PathBuf>>> = HashMap::new();
-    for file in &files {
-        if let Some(ext) = file.extension().and_then(|s| s.to_str()) {
-            if let Some((n, types)) = map_extension(ext) {
-                for typename in &types[0..(*n)] {
-                    files_by_type
-                        .entry(typename.to_string())
-                        .or_default()
-                        .push(Rc::clone(file));
-                }
-            }
-        }
-        if let Some(name) = file.file_name().and_then(|s| s.to_str()) {
-            if let Some((n, types)) = map_name(name) {
-                for typename in &types[0..(*n)] {
-                    files_by_type
-                        .entry(typename.to_string())
-                        .or_default()
-                        .push(Rc::clone(file));
-                }
-            }
-        }
-    }
-    Ok(FileSet {
-        root_path: root_path.clone(),
-        files,
-        files_by_type,
-    })
+    FileSet::from_raw_files(root_path, files_raw)
 }
